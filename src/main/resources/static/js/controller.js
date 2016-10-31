ba24ba418e01d90a2b73f65fb5d8051bc4a749c4
erikmura44 <erikmura44@gmail.com>
2016-10-31 14:35:44 -0600

    renamed controllers

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,8 +1,4 @@
-app.controller('usersController', function($scope) {
-    $scope.headingTitle = "User List";
-});
-
-app.controller('rolesController', function($scope, $http) {
+app.controller('homeController', function($scope, $http) {
     $scope.view = {};
     $scope.headingTitle = "Roles List";
     $http.get('http://localhost:8080/barinfo')
