db388c992290e7cefc1b54e4d95f57426c513d20
Lisa Hull <lisa@jchull.com>
2016-10-31 12:41:21 -0600

    showing data :sparkles:

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -2,6 +2,11 @@ app.controller('usersController', function($scope) {
     $scope.headingTitle = "User List";
 });
 
-app.controller('rolesController', function($scope) {
+app.controller('rolesController', function($scope, $http) {
+    $scope.view = {};
     $scope.headingTitle = "Roles List";
+    $http.get('http://localhost:8080/barinfo')
+        .then(function(data){
+            $scope.view.data = data.data;
+        })
 });
