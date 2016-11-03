6dadf6acb69e58380cc3ffc08a273019508b9f75
Lisa Hull <lisa@jchull.com>
2016-11-03 13:45:16 -0600

    setup instructions

--- src/main/resources/static/js/services.js
+++ src/main/resources/static/js/services.js
@@ -1,13 +1,16 @@
 angular.module('app.services',["ngResource"])
 .factory('BarInfoDS', function ($resource) {
     var data = $resource('/barinfo/:id', {barinfo: '@id'}, {
+        get: {
+            method: 'GET'
+        },
         update: {
             method: 'PUT',
             isArray:false
         },
         save: {
             method: 'POST',
-            isArray: true
+            isArray: false
         }
     });
     return data;
--- target/classes/static/js/controller.js
+++ target/classes/static/js/controller.js
@@ -5,12 +5,30 @@ angular.module('app.controller', ["app.services"])
     $scope.headingTitle = "Happy Hour";
 
     $scope.view.data = BarInfoDS.query();
+    console.log('$scope.view.data= ' , $scope.view.data);
 
     var mapOptions = {
         zoom: 12,
         center: new google.maps.LatLng(39.7392, -104.9903),
         mapTypeId: google.maps.MapTypeId.TERRAIN
     }
+
+    var addBars = function(info) {
+        var cities = [];
+        for (var i = 0; i < info.length; i++) {
+            cities.push({
+                restaurant: info[i].restaurantName,
+                neighborhood: info[i].neighborhood,
+                lat: info[i].latitude,
+                long: info[i].longitude
+            })
+        }
+        for (var j = 0; j < cities.length; j++) {
+            createMarker(cities[j]);
+        }
+    }
+
+    addBars($scope.view.data);
 
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
@@ -50,22 +68,22 @@ angular.module('app.controller', ["app.services"])
 
 .controller('AddHappyHour', function ($scope, $location, BarInfoDS) {
     $scope.newHappyHour = {};
-    // $scope.checkbox = {};
-    //
-    //
-    // $scope.checkbox = {
-    //     monday : false,
-    //     tuesday : false,
-    //     wednesday : false,
-    //     thursday : false,
-    //     friday : false,
-    //     saturday : false,
-    //     sunday : false
-    // };
+    $scope.checkbox = {};
+
+
+    $scope.checkbox = {
+        monday : false,
+        tuesday : false,
+        wednesday : false,
+        thursday : false,
+        friday : false,
+        saturday : false,
+        sunday : false
+    };
 
     $scope.save = function save(){
         BarInfoDS.save($scope.newHappyHour);
-        // BarInfoDS.save($scope.checkbox);
+        BarInfoDS.save($scope.checkbox);
         $location.path("/");
     };
 
@@ -73,78 +91,78 @@ angular.module('app.controller', ["app.services"])
 
 
 //Data
-var cities = [
-    {
-        restaurant: 'Linger',
-        neighborhood: 'Lohi',
-        lat: 39.759526,
-        long: -105.0136293
-    },
-    {
-        restaurant: 'Lola',
-        neighborhood: 'Lohi',
-        lat: 39.7592294,
-        long: -105.0107865
-    },
-    {
-        restaurant: 'Dunbar Kitchen & Tap House',
-        neighborhood: 'Rino/Five Points',
-        lat: 39.756242,
-        long: -104.975893
-    },
-    {
-        restaurant: 'Central Bistro and Bar',
-        neighborhood: 'Lohi',
-        lat: 39.759193,
-        long: -105.0084136
-    },
-    {
-        restaurant: 'Old Major',
-        neighborhood: 'Lohi',
-        lat: 39.763547,
-        long: -105.010789
-    },
-    {
-        restaurant: 'Humbolt Farm-Fish-Wine',
-        neighborhood: 'Uptown',
-        lat: 39.743485,
-        long: -104.969341
-    },
-    {
-        restaurant: 'The Way Back',
-        neighborhood: 'Highlands',
-        lat: 39.769235,
-        long: -105.041319
-    },
-    {
-        restaurant: 'Larimer Lounge',
-        neighborhood: 'Curtis Park',
-        lat: 39.7599262,
-        long: -104.9838041
-    },
-    {
-        restaurant: 'Ace',
-        neighborhood: 'Uptown',
-        lat: 39.743502,
-        long: -104.980749
-    },
-    {
-        restaurant: 'Gozo',
-        neighborhood: 'South Broadway',
-        lat: 39.715948,
-        long: -104.9871319
-    },
-    {
-        restaurant: 'Vesper Lounge',
-        neighborhood: 'Central Denver',
-        lat: 39.727443,
-        long: -104.983996
-    },
-    {
-        restaurant: 'Leña',
-        neighborhood: 'South Broadway',
-        lat: 39.7171992,
-        long: -104.9873205
-    }
-];
+// var cities = [
+//     {
+//         restaurant: 'Linger',
+//         neighborhood: 'Lohi',
+//         lat: 39.759526,
+//         long: -105.0136293
+//     },
+//     {
+//         restaurant: 'Lola',
+//         neighborhood: 'Lohi',
+//         lat: 39.7592294,
+//         long: -105.0107865
+//     },
+//     {
+//         restaurant: 'Dunbar Kitchen & Tap House',
+//         neighborhood: 'Rino/Five Points',
+//         lat: 39.756242,
+//         long: -104.975893
+//     },
+//     {
+//         restaurant: 'Central Bistro and Bar',
+//         neighborhood: 'Lohi',
+//         lat: 39.759193,
+//         long: -105.0084136
+//     },
+//     {
+//         restaurant: 'Old Major',
+//         neighborhood: 'Lohi',
+//         lat: 39.763547,
+//         long: -105.010789
+//     },
+//     {
+//         restaurant: 'Humbolt Farm-Fish-Wine',
+//         neighborhood: 'Uptown',
+//         lat: 39.743485,
+//         long: -104.969341
+//     },
+//     {
+//         restaurant: 'The Way Back',
+//         neighborhood: 'Highlands',
+//         lat: 39.769235,
+//         long: -105.041319
+//     },
+//     {
+//         restaurant: 'Larimer Lounge',
+//         neighborhood: 'Curtis Park',
+//         lat: 39.7599262,
+//         long: -104.9838041
+//     },
+//     {
+//         restaurant: 'Ace',
+//         neighborhood: 'Uptown',
+//         lat: 39.743502,
+//         long: -104.980749
+//     },
+//     {
+//         restaurant: 'Gozo',
+//         neighborhood: 'South Broadway',
+//         lat: 39.715948,
+//         long: -104.9871319
+//     },
+//     {
+//         restaurant: 'Vesper Lounge',
+//         neighborhood: 'Central Denver',
+//         lat: 39.727443,
+//         long: -104.983996
+//     },
+//     {
+//         restaurant: 'Leña',
+//         neighborhood: 'South Broadway',
+//         lat: 39.7171992,
+//         long: -104.9873205
+//     }
+// ];
 
